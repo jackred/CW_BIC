803d8255a3b2e4ba4d07ce7d688b911b591b9c5c
Jackred <jackred@tuta.io>
2019-11-24 19:02:06 +0000

    ADD activations functions + clean

--- src/ann.py
+++ src/ann.py
@@ -10,29 +10,38 @@
 from ann_help import default_activation
 
 
-"""
-represent a connection between 2 layers
-"""
 class Connection:
+    """
+    represent a connection between 2 layers:
+    layer1 and layer2
+    weights is a list of weights between each neurons of the 2 layer
+    """
     def __init__(self, layer1, layer2,):
         self.layer1 = layer1
         self.layer2 = layer2
         self.weights = []
 
-    def update_weights(self, weights):
-        self.weights = weights
-
+    """
+    transfer function
+    compute the value of layer2 with the value from layer1
+    """
     def compute_layers(self):
         for i in range(self.layer2.length):
             tmp = 0
             for j in range(self.layer1.length):
-                tmp += (self.layer1.neurons[j]
-                        * self.weights[j][i]
-                        + self.layer2.bias)
+                tmp += (self.layer1.neurons[j]  # activation of layer1
+                        * self.weights[j][i]  # multiply by the weights
+                        + self.layer2.bias)  # add the bias of layer2
+            # activation of layer2
             self.layer2.neurons[i] = self.layer2.activation(tmp)
 
 
 class Layer:
+    """
+    represent a layer of neurons
+    contain N neurons
+    is associated to a bias and an activation function
+    """
     def __init__(self, nb_neurons, activation):
         self.neurons = [0] * nb_neurons
         self.length = nb_neurons
@@ -41,14 +50,22 @@ class Layer:
 
 
 class InputLayer(Layer):
+    """
+    represent the input layer
+    contain N neurons
+    """
     def __init__(self, nb_neurons):
         self.neurons = [0] * nb_neurons
         self.length = nb_neurons
 
 
 class ANN:
-    def __init__(self, nb_neurons, nb_layers, activation):
-        self.nb_neurons = sum(nb_neurons[i] * nb_neurons[i+1]
+    """
+    Artificial neural network
+    Contain [N0, N1, ..., Nn-1] neurons, with n layers
+    """
+    def __init__(self, nb_neurons, nb_layers, activations):
+        self.nb_weights = sum(nb_neurons[i] * nb_neurons[i+1]
                               for i in range(len(nb_neurons)-1))
         if nb_layers < 0:
             raise ValueError('There should be at least one layer')
@@ -57,36 +74,51 @@ class ANN:
         if len(nb_neurons) != nb_layers:
             raise ValueError(
                 'Numbers of neurons should match numbers of layers')
-        self.layers = [Layer(nb_neurons[i], activation)
+        self.layers = [Layer(nb_neurons[i], activations[i])
                        for i in range(1, nb_layers)]
         self.layers.insert(0, InputLayer(nb_neurons[0]))
         self.connections = [Connection(self.layers[i],
                                        self.layers[i+1])
                             for i in range(nb_layers - 1)]
-
+    """
+    evaluate the given inputs throught the neural network
+    compute the value and return a result
+    """
     def activation(self, input_nodes):
         self.connections[0].layer1.neurons = input_nodes
         for connection in self.connections:
             connection.compute_layers()
         return self.layers[-1].neurons
 
+    """
+    replace the weights (training)
+    """
     def update_weights(self, weights):
         weights = self.format_weights(weights)
         for i in range(len(self.connections)):
-            self.connections[i].update_weights(weights[i])
+            self.connections[i].weights = weights[i]
 
+    """
+    replace the bias (training)
+    """
     def update_bias(self, bias):
         for i in range(len(self.layers)-1):
             self.layers[i+1].bias = bias[i]
 
+    """
+    format the returned params and update them (training)
+    """
     def update_params(self, params):
         m = 0
-        weights = params[m:self.nb_neurons]
-        m += self.nb_neurons
-        bias = params[m:m+len(self.layers)-1]
+        weights = params[m:self.nb_weights]
+        m += self.nb_weights
+        bias = params[m:m+len(self.layers)-1]  # -1: no bias on input layer
         self.update_weights(weights)
         self.update_bias(bias)
 
+    """
+    format the weights according to the number of neurons per layer
+    """
     def format_weights(self, weights):
         res = []
         j = 0
